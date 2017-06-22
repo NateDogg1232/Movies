6c5ff80dadb46352481b824a6e4873bd9204ab12
NateDogg1232 <nathanielbarragan@gmail.com>
2017-06-22 00:15:43 -0700

    Added everything

---
+++ app/src/main/java/nathaniel/movies/addMovie.kt
@@ -0,0 +1,16 @@
+package nathaniel.movies
+
+import android.database.sqlite.SQLiteDatabase
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class addMovie : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_movie)
+
+    }
+}
